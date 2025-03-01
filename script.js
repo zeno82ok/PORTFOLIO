3977e7a0039f0380e473344c2a83ca0d8c9eed23
Bikash Shaw <raaz1535y@gmail.com>
2025-03-01 17:40:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Bikash Shaw","Student", "Programmer", "Frontend Developer"];
+const textArray = ["Bikash Shaw","a Student", "a Programmer", "a Frontend Developer"];
 const typingDelay = 100;
 const erasingDelay = 50;
 const newTextDelay = 1000; // Delay between current and next text
